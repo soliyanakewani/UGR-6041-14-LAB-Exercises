f9e5ee919c4550e3ea627e6b0cf5e6488a9c52da
soliyana <soliyanakewani228@gmail.com>
2024-03-08 16:16:18 +0300

    files

---
+++ LAB-1-VariablesandDatatypes1.dart
@@ -0,0 +1,10 @@
+void main() {
+  
+  String name = "Soliyana Kewani";
+  int age = 21;
+  String favColor = "black";
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Favorite Color: $favColor');
+}
